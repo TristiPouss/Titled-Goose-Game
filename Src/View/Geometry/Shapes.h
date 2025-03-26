d471dfd9eac86f1f97eb0725d92a464f30840c3c
TristiPouss <tristandsg@gmail.com>
2025-03-26 17:23:15 +0100

    name refactors

--- Src/View/Geometry/Shapes.h
+++ Src/View/Geometry/Shapes.h
@@ -13,6 +13,6 @@
 /*
     Function to draw a edge_length*edge_length cube with a facetisation level of facet_number
 */
-void draw_cube(float edge_length, int facet_number);
+void drawCube(float edgeLength, int facetNumber);
 
 #endif // !SHAPES_H
