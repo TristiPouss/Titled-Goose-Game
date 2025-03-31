8efa78032d9972ad52694711f9a64c0ceb275fc0
TristiPouss <tristandsg@gmail.com>
2025-03-31 15:39:40 +0200

    table, colors

--- Src/View/Camera/Camera.cpp
+++ Src/View/Camera/Camera.cpp
@@ -4,7 +4,7 @@
 static float isometricCamera[3][3] = { 
     { 0.0,  25 + c, 0.0},
     { 0.0,  0.0,   -100.0},
-    { 0.0,  1.0,   0.0} 
+    { 0.0,  1.0,    0.0} 
 };
 
 static float (*cam)[3][3] = &isometricCamera; // in case we want to make multiple preconfigured cameras
@@ -13,20 +13,20 @@ static double dist = sqrt(pow((*cam)[1][0] - (*cam)[0][0], 2) + pow((*cam)[1][1]
 static double ray = c;
 static double ang = asin(ray / dist) * 2 * 180 / M_PI;
 
-void initCamera(bool isCameraPerspect) {
+void initCamera(bool isCameraPerspect, int wx, int wy) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    double ratio = (double)wTx / wTy;
+    double ratio = (double)wx / wy;
     if (!isCameraPerspect) {
-        if (wTx > wTy)
-            glOrtho((-10.0 - c) * ratio, (10.0 + c) * ratio, -10.0 - c, 10.0 + c, -25.0 - c, 25.0 + c);
+        if (wx > wy)
+            glOrtho((-10.0 - c) * ratio, (10.0 + c) * ratio, -10.0 - c, 10.0 + c, -100.0 - c, 200.0 + c);
         else
-            glOrtho(-10.0 - c, 10.0 + c, (-10.0 - c) / ratio, (10.0 + c) / ratio, -25.0 - c, 25.0 + c);
+            glOrtho(-10.0 - c, 10.0 + c, (-10.0 - c) / ratio, (10.0 + c) / ratio, -100.0 - c, 200.0 + c);
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
     }
     else {
-        gluPerspective(ang, ratio, dist - ray - 20, (dist + ray)*2);
+        gluPerspective(ang, ratio, dist - ray - 20, (dist + ray)*3);
         dist = sqrt(pow((*cam)[1][0] - (*cam)[0][0], 2) + pow((*cam)[1][1] - (*cam)[0][1], 2) + pow((*cam)[1][2] - (*cam)[0][2], 2));
         ang = asin(ray / dist) * 2 * 180 / M_PI;
         glMatrixMode(GL_MODELVIEW);
