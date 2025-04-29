d5f3f613bc6e3cd72cc0b4ecb43adedfa70fc1f6
RizBleu <mateo.galizzi05@gmail.com>
2025-04-29 16:53:45 +0200

    camera player view

--- Src/View/Camera/Camera.cpp
+++ Src/View/Camera/Camera.cpp
@@ -7,16 +7,35 @@ static float isometricCamera[3][3] = {
     { 0.0,  1.0,    0.0} 
 };
 
+static float playerTrackingCamera[3][3] = { 
+    { 0.0,  25 + scenerySize, 0.0},
+    { 0.0,  0.0,  -100.0},
+    { 0.0,  1.0,    0.0} 
+};
+
 static float (*cam)[3][3] = &isometricCamera; // in case we want to make multiple preconfigured cameras
 
 static double dist = sqrt(pow((*cam)[1][0] - (*cam)[0][0], 2) + pow((*cam)[1][1] - (*cam)[0][1], 2) + pow((*cam)[1][2] - (*cam)[0][2], 2));
 static double ray = scenerySize;
 static double ang = asin(ray / dist) * 2 * 180 / M_PI;
 
-void initCamera(bool isCameraPerspect, int wx, int wy) {
+void setCurrentPlayerPosition(float x, float y, float z) {
+    playerTrackingCamera[1][0] = x;
+    playerTrackingCamera[1][1] = y;
+    playerTrackingCamera[1][2] = z;
+}
+
+void initCamera(bool isCameraPerspect, bool cameraOnCurrentPlayer, int wx, int wy) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     double ratio = (double)wx / wy;
+
+    if (cameraOnCurrentPlayer) {
+        cam = &playerTrackingCamera;
+    } else {
+        cam = &isometricCamera;
+    }
+
     if (!isCameraPerspect) {
         if (wx > wy)
             glOrtho((-10.0 - scenerySize) * ratio, (10.0 + scenerySize) * ratio, -10.0 - scenerySize, 10.0 + scenerySize, 0.0 - scenerySize, 1000.0 + scenerySize);
